0dc9774fe81e7d2ef1ed23b58ded297981899d7a
Alisjj <aliyussajjads@gmail.com>
2022-06-24 13:48:55 +0100

    html fronnt

--- src/server.js
+++ src/server.js
@@ -1,24 +1,36 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const authorize = require('./middlewares/authorize');
 const morgan = require('morgan');
+let {people} = require('./data');
 
 
-app.use([morgan('tiny'), authorize]);
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '../public')));
+// parse form data
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("Home")
+app.get('/api/people', (req, res) => {
+    res.status(200).json({success: true, data: people});
 });
 
-// about
-app.get('/about', (req, res) => {
-    res.send("About")
+app.post('/api/people', (req, res) => {
+    const {name} = req.body
+    if (!name){
+        return res.status(400).json({success: false, msg: 'Name is required'});
+    }
+    res.status(201).json({success: true, person: name});
 });
 
-app.get('/api/products', (req, res) => {
-    res.send("Products")
+app.post('/login', (req, res) => {
+    const {name} = req.body
+    if (name){
+        return res.status(200).send('Welcome ' + name);
+    }
+    res.status(401).send('Please enter your name');
 });
 
 app.listen(8000, () => {
-    console.log('Server is listening on port 8000....');
+    console.log('Server is running on port 8000.....');
 });
